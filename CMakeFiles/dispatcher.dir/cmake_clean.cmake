4017fa94b95a24a4d48a4220c1f87e5fe18deb7d
christian garcia <cdogg111@gmail.com>
2017-09-09 23:09:07 -0700

    ...

--- CMakeFiles/dispatcher.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "bin/libdispatcher.pdb"
-  "bin/libdispatcher.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dispatcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
